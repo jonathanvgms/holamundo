0b750554cc5e9ecd1af6d847902da90f22e3b511
jonathanvgms <jonathanvgms@gmail.com>
2025-04-28 10:03:41 -0300

    Uso de ConsoleTables

---
+++ HolaMundo/Mascota.cs
@@ -0,0 +1,8 @@
+namespace HolaMundo;
+
+public class Mascota
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+}
--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hola Mundo !!");
+﻿using ConsoleTables;
+using HolaMundo;
+
+List<Mascota> mascotas = new List<Mascota>()
+{
+    new Mascota { Id = 1, Nombre = "Perro", Edad = 5 },
+    new Mascota { Id = 2, Nombre = "Gato", Edad = 3 },
+    new Mascota { Id = 3, Nombre = "Loro", Edad = 2 }
+};
+
+var table = new ConsoleTable("ID", "NOMBRE", "EDAD");
+
+foreach (var mascota in mascotas)
+{
+    table.AddRow(mascota.Id, mascota.Nombre, mascota.Edad);
+}
+
+// table.AddRow(1, 2, 3)
+//      .AddRow("this line should be longer", "yes it is", "oh");
+
+
+table.Write(Format.Alternative);
+Console.WriteLine();
